75988aa675790ef174beb30d2c2051292f9e9ed7
konstantin2014 <kokos201475@gmail.com>
2022-09-22 13:38:10 +0300

    Создание списка фильмов

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,14 @@
+import Movie from "./Movie";
+
+function MovieList(props) {
+  const { movies } = props;
+
+  return (
+    <div className="movies">
+      {movies.map((movie) => (
+        <Movie key={movie.imdbID} {...movie} />
+      ))}
+    </div>
+  );
+}
+export default MovieList;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,27 @@
-function Main() {
-  return <main className="container content">Movies App</main>;
+import { Component } from "react";
+import MovieList from "../components/MovieList";
+
+class Main extends Component {
+  state = {
+    movies: [],
+  };
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?s=Matrix&apikey=7c931730")
+      .then((responce) => responce.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+  render() {
+    const { movies } = this.state;
+    return (
+      <main className="container content">
+        {movies.length ? (
+          <MovieList movies={this.state.movies} />
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </main>
+    );
+  }
 }
 
 export default Main;
